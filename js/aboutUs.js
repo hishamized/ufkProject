2bbd0cf713c202c25ae3d1104de600fd7b190b05
hishamized <mirhashim2001@gmail.com>
2024-04-08 02:09:17 +0530

    Following Updates: 1) About us page UI updated 2) Embedded social media profiles on home page added

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     var navbarHeight = document.getElementById("navbar").offsetHeight;
     var project = document.getElementsByClassName("about-us-container")[0];
-      project.style.marginTop = navbarHeight + 50 + "px";
+      project.style.marginTop = navbarHeight + "px";
   });
 
